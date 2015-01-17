1bb236efa95e9f1c5672bacccf4cf3ca6a4e37bd
Peter Trško <peter.trsko@gmail.com>
2015-01-17 19:37:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Toolbox
+=======
+
+[![Haskell Programming Language](https://img.shields.io/badge/language-Haskell-blue.svg)][Haskell.org]
+[![BSD3 License](http://img.shields.io/badge/license-BSD3-brightgreen.svg)][tl;dr Legal: BSD3]
+
+
+Description
+-----------
+
+Tray icon with configurable popup menu.
+
+
+
+[Haskell.org]:
+  http://www.haskell.org
+  "The Haskell Programming Language"
+[tl;dr Legal: BSD3]:
+  https://tldrlegal.com/license/bsd-3-clause-license-%28revised%29
+  "BSD 3-Clause License (Revised)"
---
+++ src/Main.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+module Main (main)
+  where
+
+import Control.Applicative ((<$>))
+import Control.Monad (when)
+import Data.Maybe (fromJust)
+import Data.Typeable (Typeable)
+import System.Command (cmd)
+import System.Exit (ExitCode(..), exitSuccess)
+
+import System.FilePath ((</>))
+
+import Data.String.Here.Interpolated (i)
+import Graphics.UI.Gtk (initGUI, mainGUI, on)
+import Graphics.UI.Gtk.Display.StatusIcon
+    ( statusIconNewFromFile
+    , statusIconActivate
+    , statusIconPopupMenu
+    )
+import Graphics.UI.Gtk.ActionMenuToolbar.Action (actionNew, actionActivated)
+import Graphics.UI.Gtk.ActionMenuToolbar.ActionGroup
+    ( ActionGroup
+    , actionGroupNew
+    , actionGroupAddAction
+    )
+import Graphics.UI.Gtk.ActionMenuToolbar.UIManager
+    ( uiManagerNew
+    , uiManagerAddUiFromString
+    , uiManagerGetWidget
+    , uiManagerInsertActionGroup
+    )
+import Graphics.UI.Gtk.MenuComboToolbar.Menu (castToMenu, menuPopup)
+
+import Paths_toolbox (getDataFileName)
+
+
+data MenuItem = MenuItem
+    { menuId     :: String
+    , menuText   :: String
+    , menuAction :: IO ()
+    }
+  deriving (Typeable)
+
+menuItems :: [MenuItem]
+menuItems =
+    [ MenuItem "something" "Something"    (cmd "false" >>= printResult)
+    , MenuItem "exit"      "Exit toolbox" exitSuccess
+    ]
+  where
+    printResult ec = when (ec /= ExitSuccess) $ print ec
+
+menuItemsXml :: String
+menuItemsXml = concatMap menuItemToXml menuItems
+  where
+    menuItemToXml MenuItem{menuId} = [i| <menuitem action="${menuId}" /> |]
+
+popupMenuXml :: String
+popupMenuXml = [i| <popup>${menuItemsXml}</popup> |]
+
+main :: IO()
+main = do
+    args <- initGUI
+    print args -- TODO
+
+    iconFile <- getDataFileName $ "icons" </> iconFileName
+    icon <- statusIconNewFromFile iconFile
+
+    group <- actionGroupNew "PopupMenuActions"
+    mapM_ (createMenuItem group) menuItems
+
+    manager <- uiManagerNew
+    _ <- uiManagerAddUiFromString manager popupMenuXml
+    uiManagerInsertActionGroup manager group 0
+
+    popupMenu <- castToMenu . fromJust <$> uiManagerGetWidget manager "/popup"
+    _ <- on icon statusIconActivate $ menuPopup' popupMenu
+    _ <- on icon statusIconPopupMenu $ (\menu _ _ -> menuPopup' menu) popupMenu
+
+    mainGUI
+  where
+    menuPopup' = flip menuPopup Nothing
+
+    createMenuItem :: ActionGroup -> MenuItem -> IO ()
+    createMenuItem group MenuItem{..} = do
+        action <- actionNew menuId menuText Nothing Nothing
+        _ <- on action actionActivated menuAction
+        actionGroupAddAction group action
+
+    iconFileName = "elegantthemes-tools-icon.png"
+--  iconFileName = "elegantthemes-beautiful-flat-icons-tools-icon.png"
