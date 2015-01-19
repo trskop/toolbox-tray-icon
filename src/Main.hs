43f9ccf0edc54bbea19328036ee8f6fb23d38659
Peter Trško <peter.trsko@gmail.com>
2015-01-19 21:19:22 +0100

    Functioning tray icon with exampe config file

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,37 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  Tray icon with configurable popup menu.
+-- Copyright:    (c) 2015, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    experimental
+-- Portability:  NoImplicitPrelude
+--
+-- Tray icon with configurable popup menu.
 module Main (main)
   where
 
-import Control.Monad (Monad((>>=)), mapM_, when)
-import Data.Eq (Eq((/=)))
+import Control.Exception (SomeException, handle)
+import Control.Monad (Monad(return, (>>=)), (=<<), mapM_, void)
+import Data.Bool (otherwise)
+import Data.Either (Either(Left, Right))
+import Data.Eq (Eq((==)))
 import Data.Function ((.), ($), flip)
 import Data.Functor ((<$>))
-import Data.List (concatMap)
+import Data.List ((++), concatMap)
 import Data.Maybe (Maybe(Nothing), fromJust)
 import Data.String (String)
-import Data.Typeable (Typeable)
-import System.Command (cmd)
-import System.Exit (ExitCode(ExitSuccess), exitSuccess)
-import System.IO (IO, print)
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (IO, hPutStrLn, stderr)
+import Text.Show (Show(show))
 
 import System.FilePath ((</>))
+import System.Process (system)
 
+import Control.Lens ((^.), foldMapOf)
 import Data.String.Here.Interpolated (i)
 import Graphics.UI.Gtk (initGUI, mainGUI, on)
 import Graphics.UI.Gtk.Display.StatusIcon
@@ -40,61 +52,76 @@ import Graphics.UI.Gtk.ActionMenuToolbar.UIManager
     , uiManagerInsertActionGroup
     )
 import Graphics.UI.Gtk.MenuComboToolbar.Menu (castToMenu, menuPopup)
+import Options.Applicative (fullDesc)
+
+import Main.ConfigFile (readConfigFile)
+import Main.Options (execParser, optionsParser)
+import Main.Type.MenuItem (MenuItem(MenuItem), MenuItems)
+import qualified Main.Type.MenuItem as MenuItem (MenuItem(id))
+import Main.Type.MenuItem.Lens (menuItems)
+import qualified Main.Type.MenuItem.Lens as MenuItem.Lens (command, id, name)
+import qualified Main.Type.Options.Lens as Options (iconFile)
 
 import Paths_toolbox (getDataFileName)
 
 
-data MenuItem = MenuItem
-    { menuId     :: String
-    , menuText   :: String
-    , menuAction :: IO ()
-    }
-  deriving (Typeable)
-
-menuItems :: [MenuItem]
-menuItems =
-    [ MenuItem "something" "Something"    (cmd "false" >>= printResult)
-    , MenuItem "exit"      "Exit toolbox" exitSuccess
-    ]
+popupMenuXml :: MenuItems -> String
+popupMenuXml items = [i|
+    <popup>
+      ${menuItemsXml}
+      <menuitem action="exit" />
+    </popup> |]
   where
-    printResult ec = when (ec /= ExitSuccess) $ print ec
-
-menuItemsXml :: String
-menuItemsXml = concatMap menuItemToXml menuItems
-  where
-    menuItemToXml MenuItem{menuId} = [i| <menuitem action="${menuId}" /> |]
-
-popupMenuXml :: String
-popupMenuXml = [i| <popup>${menuItemsXml}</popup> |]
+    menuItemsXml = flip (foldMapOf menuItems) items . concatMap
+        $ \MenuItem{MenuItem.id = itemId} ->
+            [i| <menuitem action="${itemId}" /> |]
 
 main :: IO()
 main = do
-    args <- initGUI
-    print args -- TODO
+    options <- initGUI >>= execParser optionsParser fullDesc
 
-    iconFile <- getDataFileName $ "icons" </> iconFileName
-    icon <- statusIconNewFromFile iconFile
+    menu <- readConfigFile getDataFileName options >>= \r -> case r of
+        Right x  -> return x
+        Left msg -> do
+            hPutStrLn stderr $ "Parsing configuration failed: " ++ msg
+            exitFailure
+
+    icon <- statusIconNewFromFile =<< case options ^. Options.iconFile of
+        fileName@(c : _)
+          | c == '/'  -> return fileName
+          | otherwise -> getDataFileName fileName
+        ""            -> getDataFileName $ "icons" </> iconFileName
+          where
+            iconFileName = "elegantthemes-tools-icon.png"
+--          iconFileName = "elegantthemes-beautiful-flat-icons-tools-icon.png"
 
     group <- actionGroupNew "PopupMenuActions"
-    mapM_ (createMenuItem group) menuItems
+    foldMapOf menuItems (mapM_ (createMenuItem group)) menu
+    createMenuItem' group "exit" "Exit toolbox" exitSuccess
 
     manager <- uiManagerNew
-    _ <- uiManagerAddUiFromString manager popupMenuXml
+    _ <- uiManagerAddUiFromString manager $ popupMenuXml menu
     uiManagerInsertActionGroup manager group 0
 
     popupMenu <- castToMenu . fromJust <$> uiManagerGetWidget manager "/popup"
     _ <- on icon statusIconActivate $ menuPopup' popupMenu
-    _ <- on icon statusIconPopupMenu $ (\menu _ _ -> menuPopup' menu) popupMenu
+    _ <- on icon statusIconPopupMenu $ (\m _ _ -> menuPopup' m) popupMenu
 
     mainGUI
   where
     menuPopup' = flip menuPopup Nothing
 
-    createMenuItem :: ActionGroup -> MenuItem -> IO ()
-    createMenuItem group MenuItem{..} = do
-        action <- actionNew menuId menuText Nothing Nothing
-        _ <- on action actionActivated menuAction
-        actionGroupAddAction group action
+    handleExceptions = handle $ \e ->
+        hPutStrLn stderr $ show (e :: SomeException)
 
-    iconFileName = "elegantthemes-tools-icon.png"
---  iconFileName = "elegantthemes-beautiful-flat-icons-tools-icon.png"
+    createMenuItem :: ActionGroup -> MenuItem -> IO ()
+    createMenuItem group item = createMenuItem' group
+        (item ^. MenuItem.Lens.id)
+        (item ^. MenuItem.Lens.name)
+        . handleExceptions . void . system $ item ^. MenuItem.Lens.command
+
+    createMenuItem' :: ActionGroup -> String -> String -> IO () -> IO ()
+    createMenuItem' group itemId itemName itemAction = do
+        action <- actionNew itemId itemName Nothing Nothing
+        _ <- on action actionActivated itemAction
+        actionGroupAddAction group action
